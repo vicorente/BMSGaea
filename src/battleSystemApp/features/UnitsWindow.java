5fb9a1c142eaed6a7702e347d82025dbfb820cee
vgonllo <vgonllo@ZETCGE0121.MDEF.es>
2014-09-05 13:09:25 +0200

    Modificado UnitsWindow

--- src/battleSystemApp/features/UnitsWindow.java
+++ src/battleSystemApp/features/UnitsWindow.java
@@ -5,9 +5,14 @@ import java.awt.Point;
 import java.awt.Rectangle;
 
 import gov.nasa.worldwind.WWObjectImpl;
+import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.render.DrawContext;
 import gov.nasa.worldwind.render.PreRenderable;
+import gov.nasa.worldwind.util.tree.ScrollFrame;
 import gov.nasa.worldwind.util.tree.Scrollable;
+import gov.nasa.worldwind.util.tree.TreeAttributes;
+import gov.nasa.worldwind.util.tree.TreeNode;
+import gov.nasa.worldwind.util.tree.TreePath;
 /**
  * Ventana scrollable con las operaciones para informar sobre unidades
  * @author vgonllo
@@ -15,6 +20,14 @@ import gov.nasa.worldwind.util.tree.Scrollable;
  */
 public class UnitsWindow extends WWObjectImpl implements Scrollable, PreRenderable{
 
+    /**
+     * This field is set by {@link #makeVisible(TreePath)}, and read by {@link #scrollToNode(gov.nasa.worldwind.render.DrawContext)}
+     * during rendering.
+     */
+    protected TreeNode scrollToNode;
+    
+    /** Frame that contains the tree. */
+    protected ScrollFrame frame;
 	@Override
 	public void preRender(DrawContext dc) {
 		// TODO Auto-generated method stub
@@ -45,5 +58,41 @@ public class UnitsWindow extends WWObjectImpl implements Scrollable, PreRenderab
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	 /**
+     * Scroll the frame to make a the node set in {@link #scrollToNode} node visible. Does nothing if {@link
+     * #scrollToNode} is null.
+     *
+     * @param dc Draw context.
+     */
+    protected synchronized void scrollToNode(DrawContext dc)
+    {
+        if (this.scrollToNode != null)
+        {
+            // Update the frame bounds to make sure that the frame's scroll model includes the full extent of the tree
+            ScrollFrame frame = this.getFrame();
+            frame.updateBounds(dc);
 
+            Point drawPoint = new Point(0, 0);
+            Rectangle bounds = this.findNodeBounds(this.scrollToNode, this.tree.getModel().getRoot(), dc,
+                frame.getBounds(dc).getSize(), drawPoint, 1);
+
+            // Calculate a scroll position that will bring the node to the top of the visible area. Subtract the row spacing
+            // to avoid clipping off the top of the node.
+            int scroll = (int) Math.abs(bounds.getMaxY()) - this.getActiveAttributes().getRowSpacing();
+            this.frame.getScrollBar(AVKey.VERTICAL).setValue(scroll);
+
+            this.scrollToNode = null;
+        }
+    }
+    
+    public ScrollFrame getFrame()
+    {
+        return this.frame;
+    }
+
+    protected TreeAttributes getActiveAttributes()
+    {
+        return this.activeAttributes;
+    }
 }
