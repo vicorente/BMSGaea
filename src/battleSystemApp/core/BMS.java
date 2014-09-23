3dc2e7a9b01299e77624244640a2cc08e8a55e66
vicorente <victorllorente@gmail.com>
2014-09-23 14:36:13 +0200

    ajustes desde AIRBUS

--- src/battleSystemApp/core/BMS.java
+++ src/battleSystemApp/core/BMS.java
@@ -17,64 +17,74 @@ import java.util.logging.Level;
  * @author tag
  * @version $Id: WorldWindow.java 1171 2013-02-11 21:45:02Z dcollins $
  */
-public class BMS
-{
-    static
-    {
-        System.setProperty("gov.nasa.worldwind.app.config.document",
-            "configuration/worldwindow.worldwind.xml");
-        if (Configuration.isMacOS())
-        {
-        	System.setProperty(
+public class BMS {
+	static {
+		System.setProperty("gov.nasa.worldwind.app.config.document",
+				"configuration/worldwindow.worldwind.xml");
+		if (Configuration.isMacOS()) {
+			System.setProperty(
 					"com.apple.mrj.application.apple.menu.about.name", "BMS");
-            System.setProperty("apple.laf.useScreenMenuBar", "true");
-            System.setProperty("com.apple.mrj.application.growbox.intrudes", "false");
-            System.setProperty("com.apple.mrj.application.live-resize", "true");
-            System.setProperty("apple.awt.fakefullscreen", "true");
-            String s = Configuration.getStringValue(Constants.APPLICATION_DISPLAY_NAME);
-        }
-        else if (Configuration.isWindowsOS())
-        {
-        	ProxyConfigurationManager proxyConfigurationManager = new ProxyConfigurationManager();
-    		// Autenticamos la app contra el proxy
-    		Authenticator
-    				.setDefault(new ProxyAuthenticator(
-    						proxyConfigurationManager
-    								.getProperty(proxyConfigurationManager.PROXY_USERNAME),
-    						proxyConfigurationManager
-    								.getProperty(proxyConfigurationManager.PROXY_PASSWORD)));
-    		System.getProperties().put("http.proxyHost", proxyConfigurationManager
-    				.getProperty(proxyConfigurationManager.PROXY_HOST));
-    		System.getProperties().put("http.proxyPort", proxyConfigurationManager
-    				.getProperty(proxyConfigurationManager.PROXY_PORT));
-    		System.getProperties().put("https.proxyHost", proxyConfigurationManager
-    				.getProperty(proxyConfigurationManager.PROXY_HOST));
-    		System.getProperties().put("https.proxyPort",proxyConfigurationManager
-    				.getProperty(proxyConfigurationManager.PROXY_PORT));
-            System.setProperty("sun.awt.noerasebackground", "true"); // prevents flashing during window resizing
-        }
-    }
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+			System.setProperty("com.apple.mrj.application.growbox.intrudes",
+					"false");
+			System.setProperty("com.apple.mrj.application.live-resize", "true");
+			System.setProperty("apple.awt.fakefullscreen", "true");
+			String s = Configuration
+					.getStringValue(Constants.APPLICATION_DISPLAY_NAME);
+		} else if (Configuration.isWindowsOS()) {
+			ProxyConfigurationManager proxyConfigurationManager = new ProxyConfigurationManager();
+			// Autenticamos la app contra el proxy si es necesario
+			if (!proxyConfigurationManager.getProperty(
+					proxyConfigurationManager.PROXY_USERNAME).equalsIgnoreCase(
+					"")) {
+				Authenticator
+						.setDefault(new ProxyAuthenticator(
+								proxyConfigurationManager
+										.getProperty(proxyConfigurationManager.PROXY_USERNAME),
+								proxyConfigurationManager
+										.getProperty(proxyConfigurationManager.PROXY_PASSWORD)));
+				System.getProperties()
+						.put("http.proxyHost",
+								proxyConfigurationManager
+										.getProperty(proxyConfigurationManager.PROXY_HOST));
+				System.getProperties()
+						.put("http.proxyPort",
+								proxyConfigurationManager
+										.getProperty(proxyConfigurationManager.PROXY_PORT));
+				System.getProperties()
+						.put("https.proxyHost",
+								proxyConfigurationManager
+										.getProperty(proxyConfigurationManager.PROXY_HOST));
+				System.getProperties()
+						.put("https.proxyPort",
+								proxyConfigurationManager
+										.getProperty(proxyConfigurationManager.PROXY_PORT));
+			}
+			System.setProperty("sun.awt.noerasebackground", "true"); // prevents
+																		// flashing
+																		// during
+																		// window
+																		// resizing
+		}
+	}
 
-    private static final String APP_CONFIGURATION
-        = "configuration/AppConfiguration.xml";
+	private static final String APP_CONFIGURATION = "configuration/AppConfiguration.xml";
 
-    public static void main(String[] args)
-    {
-        Controller controller = new Controller();
+	public static void main(String[] args) {
+		Controller controller = new Controller();
 
-        Dimension appSize = null;
-        if (args.length >= 2) // The first two arguments are the application width and height.
-            appSize = new Dimension(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+		Dimension appSize = null;
+		if (args.length >= 2) // The first two arguments are the application
+								// width and height.
+			appSize = new Dimension(Integer.parseInt(args[0]),
+					Integer.parseInt(args[1]));
 
-        try
-        {
-            controller.start(APP_CONFIGURATION, appSize);
-        }
-        catch (Exception e)
-        {
-            String msg = "Fatal application error";
-            controller.showErrorDialog(null, "Cannot Start Application", msg);
-            Util.getLogger().log(Level.SEVERE, msg);
-        }
-    }
+		try {
+			controller.start(APP_CONFIGURATION, appSize);
+		} catch (Exception e) {
+			String msg = "Fatal application error";
+			controller.showErrorDialog(null, "Cannot Start Application", msg);
+			Util.getLogger().log(Level.SEVERE, msg);
+		}
+	}
 }
